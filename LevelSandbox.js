194bccbb84593a0177b98cb79b5c5540ad01d050
Dimitris Gravvanis <dgravvanis@icloud.com>
2019-05-21 15:43:42 +0300

    Update LevelSandbox.js
    Persist data with LevelDB using promises.

--- LevelSandbox.js
+++ LevelSandbox.js
@@ -1,41 +1,102 @@
-/* ===== Persist data with LevelDB ==================
-|  Learn more: level: https://github.com/Level/level |
-/===================================================*/
+// LevelSandbox.js
+// Persist data with LevelDB
 
-const level = require('level');
-const chainDB = './chaindata';
+const level = require('level')
+const chainDB = './chaindata'
 
+// a LevelSandbox class
 class LevelSandbox {
 
-    constructor() {
-        this.db = level(chainDB);
-    }
+  constructor() {
+    this.db = level(chainDB)
+  }
 
-    // Get data from levelDB with key (Promise)
-    getLevelDBData(key){
-        let self = this;
-        return new Promise(function(resolve, reject) {
-            // Add your code here, remember in Promises you need to resolve() or reject()
-        });
-    }
+  // Promise => Block Object
+  // Get data from levelDB with key
+  // Returns Block Object or null
+  getLevelDBData(key) {
 
-    // Add data to levelDB with key and value (Promise)
-    addLevelDBData(key, value) {
-        let self = this;
-        return new Promise(function(resolve, reject) {
-            // Add your code here, remember in Promises you need to resolve() or reject() 
-        });
-    }
+    let self = this
+    // Return promise
+    return new Promise(function(resolve, reject) {
+      // Get block
+      self.db.get(key, function(err, value) {
+        // If block not found resolve with null
+        if (err instanceof level.errors.NotFoundError) {
+          resolve(null)
+        } else if (err) {
+          reject(err)
+        } else {
+          resolve(JSON.parse(value))
+        }
+      })
+    })
+  }
 
-    // Method that return the height
-    getBlocksCount() {
-        let self = this;
-        return new Promise(function(resolve, reject){
-            // Add your code here, remember in Promises you need to resolve() or reject()
-        });
-    }
-        
+  // Promise => value
+  // Add data to levelDB with key and value
+  addLevelDBData(key, value) {
 
+    let self = this
+    // Return promise
+    return new Promise(function(resolve, reject) {
+      // Add block
+      self.db.put(key, value, function(err) {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(value)
+        }
+      })
+    })
+  }
+
+  // Promise => Number
+  // Returns the height of the chain
+  getBlocksCount() {
+
+    let self = this
+    // Return promise
+    return new Promise(function(resolve, reject) {
+      let dataArray = []
+      // Read data entries
+      self.db.createReadStream()
+      .on('data', function (data) {
+        dataArray.push(data)
+      })
+      .on('error', function (err) {
+        reject(err)
+      })
+      .on('close', function () {
+        resolve(dataArray.length)
+      })
+    })
+  }
+
+  // Promise => String
+  // Deletes all data from storage
+  // Used for testing, to clean up the database
+  deleteAllData() {
+
+    let self = this
+    // Return promise
+    return new Promise(function(resolve, reject) {
+      // Read data entries
+      let i = 0
+      self.db.createReadStream()
+      .on('data', function (data) {
+        // Delete block
+        self.db.del(i, function(err) {if (err) reject(err)})
+        i++
+      })
+      .on('error', function (err) {
+        reject(err)
+      })
+      .on('close', function () {
+        resolve('all blocks were deleted')
+      })
+    })
+  }
 }
-
-module.exports.LevelSandbox = LevelSandbox;
+// export LevelSandbox class
+module.exports.LevelSandbox = LevelSandbox
